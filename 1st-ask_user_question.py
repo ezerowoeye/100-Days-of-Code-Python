2590faa1b9617e17a6b57a83d710429272c0072c
Ebenezer Owoeye <71957653+Hethii@users.noreply.github.com>
2023-05-31 13:01:50 +0100

    Update 1st-ask_user_question.py

--- 1st-ask_user_question.py
+++ 1st-ask_user_question.py
@@ -1,3 +1,4 @@
+#task solution from Day 1:
 #1. Create a greeting for your program.
 print("Welcome to the Band Name Generator")
 #2. Ask the user for the city that they grew up in.
@@ -8,6 +9,6 @@ pet=input("What's your pet's name? \n")
 result=("Your band name could be called "+ city + " "  + pet +"")
 #5. Make sure the input cursor shows on a new line:
 print(result)
-# Solution: https://replit.com/@appbrewery/band-name-generator-end
 
+#this ask the user some personal inf
 
