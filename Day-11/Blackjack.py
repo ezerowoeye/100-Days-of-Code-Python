713a6b23ed30161866d89aab51dadfc4b2586d72
Hethii <ezertobi@gmail.com>
2023-06-13 17:09:00 +0100

    added-blackjack

--- Day-11/Blackjack.py
+++ Day-11/Blackjack.py
@@ -1,51 +1,19 @@
-############### Blackjack Project #####################
-
-#Difficulty Normal 😎: Use all Hints below to complete the project.
-#Difficulty Hard 🤔: Use only Hints 1, 2, 3 to complete the project.
-#Difficulty Extra Hard 😭: Only use Hints 1 & 2 to complete the project.
-#Difficulty Expert 🤯: Only use Hint 1 to complete the project.
-
-############### Our Blackjack House Rules #####################
-
-## The deck is unlimited in size. 
-## There are no jokers. 
-## The Jack/Queen/King all count as 10.
-## The the Ace can count as 11 or 1.
-## Use the following list as the deck of cards:
-## cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-## The cards in the list have equal probability of being drawn.
-## Cards are not removed from the deck as they are drawn.
-## The computer is the dealer.
-
-##################### Hints #####################
-
-#Hint 1: Go to this website and try out the Blackjack game: 
-#   https://games.washingtonpost.com/games/blackjack/
-#Then try out the completed Blackjack project here: 
-#   http://blackjack-final.appbrewery.repl.run
 import random
-from replit import clear
-from art import logo
 
 the_beginning = True
 while the_beginning:
   start = input("Do you want to play a game of Blackjack? Type 'y' or 'n': ")
   if start == "y":
-    clear()
-  
-    print(logo)
     cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
     
     computer_choice = []
     user_choice = []
     
-    
     def result():
       print(f"Your cards: {user_choice}, current score: {user_result}")
       computer_first = computer_choice[0]
       print(f"Computer's first card: {computer_first}")
-      
-
+    
     def computer_guess(card):
       computer_total = 0
       run_through = [0,1]
@@ -56,7 +24,6 @@ while the_beginning:
         computer_total += computer_choice[running]
       return computer_total
 
-    
     def user_guess(cards):
       user_total = 0 
 
@@ -79,109 +46,65 @@ while the_beginning:
       should_continue = input("Type 'y' to get another card, type 'n' to pass: ").lower()
       if should_continue == "n":
         is_true = False
-
-
-      
-    
-  elif start == "n":
-    the_beginning = False
+      elif should_continue == "y":
+        user_pick = random.choice(cards)
+        user_choice.append(user_pick)
+        # print(user_choice)
+        user_index = user_choice.index(user_pick)
+        user_result += user_choice[user_index]
   
- 
-  # should_continue = input("Type 'y' to get another card, type 'n' to pass: ").lower()
+        if user_result <= 20:
+          if computer_result <= 20:
+            computer= random.choice(cards)
+            computer_choice.append(computer)
+          
+            computer_index = computer_choice.index(computer)
+            computer_result += computer_choice[computer_index]
+          result()
+        elif user_result > 21:
+          is_true = False
+      is_comp_again = True
+      while is_comp_again:
+        if computer_result <= 18:
+          computer_answer = ["y", "y", "y", "n"]
+          computer_again = random.choice(computer_answer)
 
-# computer
-    if user_result <= 20:
-      user_pick = random.choice(cards)
-      # print(f"this is user pick {user_pick}")
-      user_choice.append(user_pick)
-      # print(f"this is user choice {user_choice}")
-      user_index = user_choice.index(user_pick)
-      # print(f"this is the index {user_index}")
-      user_result += user_choice[user_index]
-      result()
+          index1 = computer_answer.index(computer_again)
 
+          print(f"this is computer again = {computer_again}")
+          if computer_again == "y":
+            computer= random.choice(cards)
+            computer_choice.append(computer)
+          
+            computer_index = computer_choice.index(computer)
+            computer_result += computer_choice[computer_index]
+          else:
+            computer_result = computer_result
+            is_comp_again = False          
+        else:
+          is_comp_again= False
+        
+      
+    result()
 
-
-
-
-
-# if computer == 20:
-    # is_true = False
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#Hint 2: Read this breakdown of program requirements: 
-#   http://listmoz.com/view/6h34DJpvJBFVRlZfJvxF
-#Then try to create your own flowchart for the program.
-
-#Hint 3: Download and read this flow chart I've created: 
-#   https://drive.google.com/uc?export=download&id=1rDkiHCrhaf9eX7u7yjM1qwSuyEk-rPnt
-
-#Hint 4: Create a deal_card() function that uses the List below to *return* a random card.
-#11 is the Ace.
-#cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-
-#Hint 5: Deal the user and computer 2 cards each using deal_card() and append().
-#user_cards = []
-#computer_cards = []
-
-#Hint 6: Create a function called calculate_score() that takes a List of cards as input 
-#and returns the score. 
-#Look up the sum() function to help you do this.
-
-#Hint 7: Inside calculate_score() check for a blackjack (a hand with only 2 cards: ace + 10) and return 0 instead of the actual score. 0 will represent a blackjack in our game.
-
-#Hint 8: Inside calculate_score() check for an 11 (ace). If the score is already over 21, remove the 11 and replace it with a 1. You might need to look up append() and remove().
-
-#Hint 9: Call calculate_score(). If the computer or the user has a blackjack (0) or if the user's score is over 21, then the game ends.
-
-#Hint 10: If the game has not ended, ask the user if they want to draw another card. If yes, then use the deal_card() function to add another card to the user_cards List. If no, then the game has ended.
-
-#Hint 11: The score will need to be rechecked with every new card drawn and the checks in Hint 9 need to be repeated until the game ends.
-
-#Hint 12: Once the user is done, it's time to let the computer play. The computer should keep drawing cards as long as it has a score less than 17.
-
-#Hint 13: Create a function called compare() and pass in the user_score and computer_score. If the computer and user both have the same score, then it's a draw. If the computer has a blackjack (0), then the user loses. If the user has a blackjack (0), then the user wins. If the user_score is over 21, then the user loses. If the computer_score is over 21, then the computer loses. If none of the above, then the player with the highest score wins.
-
-#Hint 14: Ask the user if they want to restart the game. If they answer yes, clear the console and start a new game of blackjack and show the logo from art.py.
+  print(f"Your final hand: {user_choice}, final score: {user_result}")
+  print(f"Computer's final hand: {computer_choice}, final score: {computer_result}")
+  # print(f"User result = {user_result}, Computer result = {computer_result}")
+  if computer_result == 21 and user_result != 21:
+    print("The computer has a blackjack, You lose")
+  elif user_result == 21 and computer_result != 21:
+    print("You have blackjack, You Win")
+  elif user_result > computer_result and user_result < 21:
+    print("You Win!")
+  elif computer_result > user_result and computer_result < 21:
+    print("You lose.")  
+  elif user_result > 21 and computer_result > user_result:
+    print("Computer went over. You win")
+  elif computer_result > 21 and user_result > computer_result:
+    print("You went over. You lose")
+  elif user_result < 21 and computer_result > 21:
+    print("Computer went over. You win")
+  elif computer_result < 21 and user_result > 21:
+    print("You went over. You lose")
+  else:
+    print("You Draw")
