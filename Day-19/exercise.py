03b4588bf4f6aa609d3cd797db4129695ca38fc3
Hethii <ezertobi@gmail.com>
2023-06-26 23:29:49 +0100

    add_day19

--- Day-19/exercise.py
+++ Day-19/exercise.py
@@ -9,8 +9,25 @@ def move_forwards():
 
 def move_backwards():
     avia.backward(10)
+def turn_left():
+    new_heading = avia.heading() + 10
+    avia.setheading(new_heading)
+
+def turn_right():
+    new_heading = avia.heading() - 10
+    avia.setheading(new_heading)
+def clear():
+    avia.clear()
+    avia.penup()
+    avia.home()
+    avia.pendown()
+
+
 
 screen.listen()
-screen.onkey(key="w", fun=move_forwards)
-screen.onkey(key="s", fun=move_backwards)
+screen.onkey(move_forwards, "Up")
+screen.onkey(move_backwards, "Down")
+screen.onkey(clear, "c")
+screen.onkey(turn_left, "Left")
+screen.onkey(turn_right, "Right")
 screen.exitonclick()
