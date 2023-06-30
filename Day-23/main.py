d0275f9d6855ce56c6de89f1c917f14d859674d9
Hethii <ezertobi@gmail.com>
2023-06-30 14:08:54 +0100

    add_day23

--- Day-23/main.py
+++ Day-23/main.py
@@ -8,7 +8,36 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
+player = Player()
+car = CarManager()
+score = Scoreboard()
+
+
+screen.listen()
+screen.onkey(player.go_up, "Up")
+screen.onkey(player.move_right, "Right")
+screen.onkey(player.move_left, "Left")
+screen.onkey(player.go_down, "Down")
+
+
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+    car.create_car()
+    car.move_cars()
+
+    # player detection of brick
+    for the_car in car.all_cars:
+        if the_car.distance(player) < 20:
+            game_is_on = False
+            score.game_over()
+
+    # player detection of the final wall
+    if player.finish_line():
+        player.reset_position()
+        score.increase_level()
+        car.level_up()
+
+
+screen.exitonclick()
--- Day-23/player.py
+++ Day-23/player.py
@@ -1,7 +1,39 @@
+from turtle import Turtle
+
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
 
-class Player:
-    pass
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+
+        self.shape("turtle")
+        self.penup()
+        self.reset_position()
+        self.left(90)
+
+    def go_up(self):
+        self.forward(MOVE_DISTANCE)
+
+    def go_down(self):
+        new_y = self.ycor() - MOVE_DISTANCE
+        self.goto(self.xcor(), new_y)
+
+    def move_right(self):
+        new_x = self.xcor() + MOVE_DISTANCE
+        self.goto(new_x, self.ycor())
+
+    def move_left(self):
+        new_x = self.xcor() - MOVE_DISTANCE
+        self.goto(new_x, self.ycor())
+
+    def reset_position(self):
+        self.goto(STARTING_POSITION)
+
+    def finish_line(self):
+        if self.ycor() > FINISH_LINE_Y:
+            return True
+        else:
+            return False
