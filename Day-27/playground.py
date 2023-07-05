441426f21cfd7dcb1a54255cdfc3d0cd38f83fbd
Hethii <ezertobi@gmail.com>
2023-07-05 14:47:56 +0100

    Add_Day-27

---
+++ Day-27/playground.py
@@ -0,0 +1,148 @@
+# sum using unlimited arguments
+# *args: usually has type as tuple
+# def add(*args):
+#     sum = 0
+#     print(args)
+#     for n in args[0:2]:
+#         sum += n
+#     print(sum)
+#     # return sum
+#
+# add(2,5,6,8)
+# print(add(2,3,4))
+
+
+# **kwargs: keyword arguments. it usually has type dict
+
+# def calculate(n, **kwargs):
+#     print(kwargs)
+#     for key, value in kwargs.items():
+#         print(key)
+#         print(value)
+#     n += kwargs["add"]
+#     n *= kwargs["multiply"]
+#     print(n)
+#
+# calculate(2, add=3, multiply = 5)
+
+
+# class Car:
+#     def __init__(self, **kw):
+#         self.make = kw.get("make")
+#         self.model = kw.get("model")
+#         self.colour = kw.get("colour")
+#         self.seats = kw.get("seats")
+#
+#
+# my_car = Car(make="Nissan", mode="GT-R")
+# print(my_car.model)
+
+# def all_aboard(a, *args, **kw):
+#     print(a, args, kw)
+#
+# # here, 4 is positional argument (a), 7,3,0 are tuple(args) and x,y are dict[ keyword arg(kwargs)]
+# all_aboard(4, 7, 3, 0, x=10, y=64)
+
+
+# Others
+
+from tkinter import *
+
+# Creating a new window and configurations
+window = Tk()
+window.title("Widget Examples")
+window.minsize(width=500, height=500)
+
+# Labels
+label = Label(text="This is old text")
+label.config(text="This is new text")
+label.pack()
+
+
+# Buttons
+def action():
+    print("Do something")
+
+
+# calls action() when pressed
+button = Button(text="Click Me", command=action)
+button.pack()
+
+# Entries
+entry = Entry(width=30)
+# Add some text to begin with
+entry.insert(END, string="Some text to begin with.")
+# Gets text in entry
+print(entry.get())
+entry.pack()
+
+# Text
+text = Text(height=5, width=30)
+# Puts cursor in textbox.
+text.focus()
+# Adds some text to begin with.
+text.insert(END, "Example of multi-line text entry.")
+# Get's current value in textbox at line 1, character 0
+print(text.get("1.0", END))
+text.pack()
+
+
+# Spinbox
+def spinbox_used():
+    # gets the current value in spinbox.
+    print(spinbox.get())
+
+
+spinbox = Spinbox(from_=0, to=10, width=5, command=spinbox_used)
+spinbox.pack()
+
+
+# Scale
+# Called with current scale value.
+def scale_used(value):
+    print(value)
+
+
+scale = Scale(from_=0, to=100, command=scale_used)
+scale.pack()
+
+
+# Checkbutton
+def checkbutton_used():
+    # Prints 1 if On button checked, otherwise 0.
+    print(checked_state.get())
+
+
+# variable to hold on to checked state, 0 is off, 1 is on.
+checked_state = IntVar()
+checkbutton = Checkbutton(text="Is On?", variable=checked_state, command=checkbutton_used)
+checked_state.get()
+checkbutton.pack()
+
+
+# Radiobutton
+def radio_used():
+    print(radio_state.get())
+
+
+# Variable to hold on to which radio button value is checked.
+radio_state = IntVar()
+radiobutton1 = Radiobutton(text="Option1", value=1, variable=radio_state, command=radio_used)
+radiobutton2 = Radiobutton(text="Option2", value=2, variable=radio_state, command=radio_used)
+radiobutton1.pack()
+radiobutton2.pack()
+
+
+# Listbox
+def listbox_used(event):
+    # Gets current selection from listbox
+    print(listbox.get(listbox.curselection()))
+
+
+listbox = Listbox(height=4)
+fruits = ["Apple", "Pear", "Orange", "Banana"]
+for item in fruits:
+    listbox.insert(fruits.index(item), item)
+listbox.bind("<<ListboxSelect>>", listbox_used)
+listbox.pack()
+window.mainloop()
--- README.md
+++ README.md
@@ -4,5 +4,14 @@ just me learning python
 Day 1 and Day 2: done May 31, 2023.
 
 
-Day 24A: done June 01
-Day 25B: done June 02
+Day 24A: done July 01
+Day 25B: done July 02
+Day 26: done July 04
+Day 27: done July 05
+
+Library learned so far:
+1. Turtle: goto(), getheading(), setheading(), listening and call function, write, color, 
+self.shape("circle"), self.penup(), self.shapesize(), self.color("blue"), self.speed("fastest"), self.refresh()
+Screen(), screen.title(), image = "", screen.addshape(image),screen.setup()
+2. Pandas: read_csv(), DataFrame
+3. Tkinter: label, entry, button, get(), pack, grid, place, 
