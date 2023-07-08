3211fccb1ff6b6c570e0e749804e19be9b4675c7
Hethii <ezertobi@gmail.com>
2023-07-08 10:26:22 +0100

    Add_Day-29

--- Day-27-convert_miles_to_km/exercise.py
+++ Day-27-convert_miles_to_km/exercise.py
@@ -1,45 +1,43 @@
-from tkinter import *
+# from tkinter import *
+#
+#
+# def button_clicked():
+#     print("I got clicked")
+#     # text = my_label.config(text=f"{input.get()}")
+#     text = input.get()
+#
+#     print(text)
+#
+#
+# window = Tk()
+# window.title("My First GUI Program")
+# window.minsize(width=500, height=300)
+# window.config(padx=100, pady=200)
+#
+# # Label
+# my_label = Label(text="I Am a Label", font=("Arial", 24, "bold"))
+# # my_label.pack()
+# my_label["text"] = "Next Text"
+# # my_label.config(text="Next Text")
+# my_label.grid(column=0, row=0)
+# my_label.config(padx=50, pady=50)
+#
+#
+#
+# # Button
+# button = Button(text="Click Me", command=button_clicked)
+# # button.pack()
+# button.grid(column=1, row=1)
+#
+# new_button = Button(text="New Button")
+# new_button.grid(column=2, row=0)
 
 
-def button_clicked():
-    print("I got clicked")
-    my_label.config(text=f"{input.get()}")
-
-
-window = Tk()
-window.title("My First GUI Program")
-window.minsize(width=500, height=300)
-window.config(padx=100, pady=200)
-
-# Label
-my_label = Label(text="I Am a Label", font=("Arial", 24, "bold"))
-# my_label.pack()
-my_label["text"] = "Next Text"
-# my_label.config(text="Next Text")
-my_label.grid(column=0, row=0)
-my_label.config(padx=50, pady=50)
-
-
-
-# Button
-button = Button(text="Click Me", command=button_clicked)
-# button.pack()
-button.grid(column=1, row=1)
-
-new_button = Button(text="New Button")
-new_button.grid(column=2, row=0)
-
-
-# Entry, like input
-input = Entry(width=10)
-print(input.get())
-# input.pack()
-input.grid(column=3, row=2)
-
-
-
-
-
-
-
-window.mainloop()
+# # Entry, like input
+# input = Entry(width=10)
+# print(input.get())
+# # input.pack()
+# input.grid(column=3, row=2)
+#
+#
+# window.mainloop()
