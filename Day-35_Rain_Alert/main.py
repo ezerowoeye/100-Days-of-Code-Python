18f6d03dc3fee36a8b1b52ef95307de5a58d28be
Hethii <ezertobi@gmail.com>
2023-07-17 13:53:03 +0100

    Add_Day-36

--- Day-35_Rain_Alert/main.py
+++ Day-35_Rain_Alert/main.py
@@ -66,6 +66,6 @@ if will_rain:
 #                      to='+15558675310'
 #                  )
 #
-# print(message.sid, print)
+# print(message.sid)
 # set TWILIO_ACCOUNT_SID=AC3fded91ae307f2e8a163108ef2f2bb22
 # set TWILIO_AUTH_TOKEN=adcb7e5e427aca53d46085b9b1eb4100
